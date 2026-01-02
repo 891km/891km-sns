d2a23f76eb79932bdfb8abe078a2e6a9b4e050d4
김민주 <891km@ewhain.net>
2026-01-03 02:35:07 +0900

    refactor: profileInfo variant별 컴포넌트 분리

--- src/components/profile/profile-info.tsx
+++ src/components/profile/profile-info.tsx
@@ -4,8 +4,8 @@ import ErrorMessage from "@/components/status/error-message";
 import Loader from "@/components/status/loader";
 import { ROUTES } from "@/constants/routes";
 import { Link } from "react-router";
-
-// type ProfileInfoVariant = "default" | "post-edit" | "post";
+import { UserRoundPenIcon } from "lucide-react";
+import { useSessionUserId } from "@/store/session";
 
 interface BaseProfileInfoProps {
   userId: string;
@@ -13,7 +13,7 @@ interface BaseProfileInfoProps {
 }
 
 interface PostProfileInfoProps {
-  userId: string;
+  authorId: string;
   variant: "post";
   dateText: string;
 }
@@ -21,45 +21,70 @@ interface PostProfileInfoProps {
 type ProfileInfoProps = BaseProfileInfoProps | PostProfileInfoProps;
 
 export default function ProfileInfo(props: ProfileInfoProps) {
-  const { userId, variant } = props;
-  const { data: profileData, error, isPending } = useFetchProfile(userId);
+  const { variant } = props;
 
+  if (variant === "post") return <PostProfileInfo {...props} />;
+  if (variant === "post-edit") return <PostEditProfileInfo {...props} />;
+  return <DefaultProfileInfo {...props} />;
+}
+
+// --- components
+function PostProfileInfo(props: PostProfileInfoProps) {
+  const currentUserId = useSessionUserId();
+  const { dateText, authorId } = props;
+  const { data: profileData, error, isPending } = useFetchProfile(authorId);
   if (error) return <ErrorMessage />;
   if (isPending) return <Loader />;
 
-  if (variant === "post") {
-    const { dateText } = props;
-    return (
-      <Link to={ROUTES.PROFILE_DETAIL.replace(":userId", userId)}>
-        <div className="flex flex-row items-center gap-4">
-          <img
-            src={profileData?.profile_img_url || defaultProfile}
-            alt="유저 프로필"
-            className="h-10 w-10 rounded-full object-cover"
-          />
-          <div>
-            <div className="font-medium">{profileData.nickname}</div>
-            <div className="text-muted-foreground text-sm">{dateText}</div>
-          </div>
-        </div>
-      </Link>
-    );
-  }
-
-  if (variant === "post-edit") {
-    return (
-      <div className="flex flex-row items-center gap-3">
+  return (
+    <Link to={ROUTES.PROFILE_DETAIL.replace(":userId", authorId)}>
+      <div className="flex flex-row items-center gap-4">
         <img
           src={profileData?.profile_img_url || defaultProfile}
           alt="유저 프로필"
           className="h-10 w-10 rounded-full object-cover"
         />
-        <div className="flex flex-col items-center gap-2">
-          <div className="tex-sm font-medium">{profileData.nickname}</div>
+        <div>
+          <div className="flex items-center gap-2">
+            <div className="font-medium">{profileData.nickname}</div>
+            {currentUserId === authorId && (
+              <span className="bg-muted flex h-5.5 w-5.5 items-center justify-center gap-1 rounded-full text-sm font-semibold">
+                <UserRoundPenIcon className="text-muted-foreground h-3.5 w-auto pl-px" />
+              </span>
+            )}
+          </div>
+          <div className="text-muted-foreground text-sm">{dateText}</div>
         </div>
       </div>
-    );
-  }
+    </Link>
+  );
+}
+
+function PostEditProfileInfo(props: BaseProfileInfoProps) {
+  const { userId } = props;
+  const { data: profileData, error, isPending } = useFetchProfile(userId);
+  if (error) return <ErrorMessage />;
+  if (isPending) return <Loader />;
+
+  return (
+    <div className="flex flex-row items-center gap-3">
+      <img
+        src={profileData?.profile_img_url || defaultProfile}
+        alt="유저 프로필"
+        className="h-10 w-10 rounded-full object-cover"
+      />
+      <div className="flex flex-col items-center gap-2">
+        <div className="tex-sm font-medium">{profileData.nickname}</div>
+      </div>
+    </div>
+  );
+}
+
+function DefaultProfileInfo(props: BaseProfileInfoProps) {
+  const { userId } = props;
+  const { data: profileData, error, isPending } = useFetchProfile(userId);
+  if (error) return <ErrorMessage />;
+  if (isPending) return <Loader />;
 
   return (
     <div className="flex flex-col items-center justify-center gap-5 py-10">
